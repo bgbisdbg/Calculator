e91e76f07ce80872ada62d23d87e681821715b84
Bgbisdbg <ceo@kuznet5ovda.ru>
2024-08-26 16:26:50 +0300

    Adding the data validation function and editing HTML

--- server/calculator/config_calculator.py
+++ server/calculator/config_calculator.py
@@ -3,18 +3,30 @@ import re
 
 class ExpressionCalculation:
     def __init__(self):
-        self.precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '(': 0} # Определение приоритетов операторов
+        self.precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '(': 0}  # Определение приоритетов операторов
 
-    def split_expression(self, expression): # Функция для разделения строки
+    def split_expression(self, expression):  # Функция для разделения строки
         expression = expression.replace(' ', '')
         # С помощью регулярного выражения разбиваем строку по операторам.
         tokens = re.findall(r'\d+|\+|\-|\*|\/|\(|\)', expression)
         new_tokens = []
-        for i in range(len(tokens)): # Цикл для определения неявного умножения
+        for i in range(len(tokens)):  # Цикл для определения неявного умножения
             if tokens[i] == '(' and i > 0 and (tokens[i - 1].isdigit() or tokens[i - 1] == ')'):
                 new_tokens.append('*')
             new_tokens.append(tokens[i])
         return new_tokens
+
+    def validate_expression(self, tokens): # Добавил функцию для проверки на наличее скобок в выражение
+        stack = []
+        for token in tokens:
+            if token == '(':
+                stack.append(token)
+            elif token == ')':
+                if not stack or stack[-1] != '(':
+                    raise ValueError("Некорректное выражение: несогласованные скобки")
+                stack.pop()
+        if stack:
+            raise ValueError("Некорректное выражение: несогласованные скобки")
 
     def conversion_tokens(self, tokens):
         numbers = []
@@ -22,11 +34,11 @@ class ExpressionCalculation:
 
         i = 0
         while i < len(tokens):
-            if tokens[i].isdigit(): # Если текущий токен — число, добавляем его в стек чисел
-                numbers.append(int(tokens[i]))
-            elif tokens[i] == '(': # Если текущий токен — открывающая скобка, добавляем её в стек операторов
+            if tokens[i].isdigit():  # Если текущий токен — число, добавляем его в стек чисел
+                numbers.append(float(tokens[i]))
+            elif tokens[i] == '(':  # Если текущий токен — открывающая скобка, добавляем её в стек операторов
                 operators.append(tokens[i])
-            elif tokens[i] == ')': # Если текущий токен — закрывающая скобка, выполняем все операции до открывающей скобки
+            elif tokens[i] == ')':  # Если текущий токен — закрывающая скобка, выполняем все операции до открывающей скобки
                 while operators and operators[-1] != '(':
                     self.apply_top_operator(numbers, operators)
                 operators.pop()
@@ -43,13 +55,15 @@ class ExpressionCalculation:
 
         return numbers[0]
 
-    def apply_top_operator(self, numbers, operators): #Выполняем операции
+    def apply_top_operator(self, numbers, operators):  # Выполняем операции
+        if len(numbers) < 2:
+            raise ValueError("Некорректное выражение: недостаточно операндов для операции") # Добавил вывод если выражение не полное
         b = numbers.pop()
         a = numbers.pop()
         op = operators.pop()
         numbers.append(self._apply_operator(a, b, op))
 
-    def _apply_operator(self, a, b, op): # Операции
+    def _apply_operator(self, a, b, op):  # Операции
         if op == '+':
             return a + b
         elif op == '-':
@@ -59,12 +73,15 @@ class ExpressionCalculation:
         elif op == '/':
             if b == 0:
                 raise ValueError("Делить на ноль нельзя")
-            return a // b
+            return a / b
 
     def evaluate(self, expression):
         try:
             tokens = self.split_expression(expression)
+            self.validate_expression(tokens)
             result = self.conversion_tokens(tokens)
             return result
         except Exception as e:
             return str(e)
+
+
